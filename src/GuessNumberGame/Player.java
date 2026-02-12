fbc64e2c0128c176ad5ac59654cfd51dd1515d60
Heescheo <heescheo@gmail.com>
2026-02-12 11:23:04 +0100

    Player class finished.

--- src/GuessNumberGame/Player.java
+++ src/GuessNumberGame/Player.java
@@ -4,5 +4,18 @@ package GuessNumberGame;
 //Reading user input
 //Returning the player's guess
 
+//Importere scanner
+import java.util.Scanner;
+
 public class Player {
+
+    //Opretter ny scanner
+    private Scanner scanner = new Scanner(System.in);
+
+    public int getGuess(String prompt) {
+        System.out.print(prompt + " ");
+        return scanner.nextInt();
+    }
+
+
 }
