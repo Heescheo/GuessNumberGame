68fa706e63347554eb74071f9a65311445b3fcd7
holmganga <siggi.sejr@gmail.com>
2026-02-12 11:26:16 +0100

    getNumber() og checkGuess() sat op

--- src/GuessNumberGame/Game.java
+++ src/GuessNumberGame/Game.java
@@ -5,5 +5,26 @@ package GuessNumberGame;
 //Checking whether the guess is correct
 //Giving feedback (too high / too low / correct)
 
+import java.util.Random;
+
 public class Game {
+    Random random = new Random(); // initialiserer random
+    int randNumb; //initialiserer en int med ingen værdi (null)
+
+    //metode der generere et tal mellem
+public int getNumber(){
+    int randNumb = random.nextInt(10);
+    return randNumb;
+}
+    //metode med if statement der tjekker guess mod det genererede tal
+public String checkGuess(){
+    if (guess > randNumb){
+        return "Too high";
+    }else if (guess < randNumb){
+        return "Too low";
+    }
+    else {
+        return "Correct";
+    }
+}
 }
