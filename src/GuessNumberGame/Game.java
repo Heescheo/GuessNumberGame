8647d0e68c616a9670b4abce7e2589b76a1358f1
holmganga <siggi.sejr@gmail.com>
2026-02-12 11:43:56 +0100

    små rettelser

--- src/GuessNumberGame/Game.java
+++ src/GuessNumberGame/Game.java
@@ -10,19 +10,21 @@ import java.util.Random;
 public class Game {
     Random random = new Random(); // initialiserer random
 
-    //metode der generere et tal mellem
-public int getNumber(){
-    return random.nextInt(10);
-}
+    //metode der generere et tal mellem 0-10
+    public int getNumber() {
+        return random.nextInt(11);
+    }
+
     //metode med if statement der tjekker guess mod det genererede tal
-public String checkGuess(int guess){
-    if (guess > getNumber()){
-        return "Too high";
-    }else if (guess < getNumber()){
-        return "Too low";
+    public String checkGuess(int guess) {
+        if (guess > getNumber()) {
+            return "Too high";
+        } else if (guess < getNumber()) {
+            return "Too low";
+        } else if (guess == getNumber()) {
+            return "Correct";
+        } else {
+            return "Error";
+        }
     }
-    else {
-        return "Correct";
-    }
-}
 }
