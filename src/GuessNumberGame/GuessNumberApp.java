5ce04083dbffb739df7d103c2d8f7d071b68de1c
Rasmusfp <rap01001@stud.ek.dk>
2026-02-12 11:34:23 +0100

    hej

--- src/GuessNumberGame/GuessNumberApp.java
+++ src/GuessNumberGame/GuessNumberApp.java
@@ -6,4 +6,22 @@ package GuessNumberGame;
 //Running the game loop
 
 public class GuessNumberApp {
+    public static void main(String[] args) {
+
+    Game game = new Game();
+    Player player = new Player();
+
+
+        System.out.println("Welcome to the guessing game!");
+        System.out.println("The number is between 1-10!");
+        System.out.println("Guess the random number!");
+
+        int numGuesses = 0;
+        boolean isHit = true;
+
+        while(isHit) {
+            int guess = player.getGuess("Enter your guess: ");
+            numGuesses++;
+        }
+    }
 }
